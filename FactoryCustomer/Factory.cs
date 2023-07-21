d7e4e2c565f387563898e51e7b87ca3f5add50e0
Jagdamb <Jagdamb@DESKTOP-1ODEB72>
2023-07-21 18:19:41 +0530

    Add project files.

---
+++ FactoryCustomer/Factory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MiddleLayer;
+using Microsoft.Practices.Unity;
+using InterfaceCustomer;
+using ValidationAlgorithms;
+using InterfaceDal;
+namespace FactoryCustomer
+{
+    public static class Factory<AnyType> // Design pattern :- Simple factory pattern
+    {
+        private static IUnityContainer ObjectsofOurProjects = null;
+            
+        
+        public static AnyType Create(string Type)
+        {
+            // Design pattern :- Lazy loading. Eager loading
+            if (ObjectsofOurProjects == null)
+            {
+                ObjectsofOurProjects = new UnityContainer();
+                ObjectsofOurProjects.RegisterType<ICustomer, Customer>
+                                ("Customer", 
+                                new InjectionConstructor(
+                                    new CustomerValidationAll()));
+                ObjectsofOurProjects.RegisterType<ICustomer, Lead>
+                                    ("Lead"
+                                    , new InjectionConstructor(
+                                        new LeadValidation()));
+               
+            }
+            //Design pattern :-  RIP Replace If with Poly
+            return ObjectsofOurProjects.Resolve<AnyType>(Type); 
+        }
+    }
+}
